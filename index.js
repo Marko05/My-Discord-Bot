439531109b744917638edd28f8cb9ee22cf18890
Marko05 <Markoklincov781@gmail.com>
2020-05-02 18:29:47 +0200

    212

--- index.js
+++ index.js
@@ -2,21 +2,8 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const prefix = '!';
 
- 
-bot.on('ready', () => {
-    console.log(`${bot.user.tag} successfully logged in!`)
-    bot.user.setActivity('!test', ({type: "PLAYING"}))
-})
- 
-bot.on('message', message => {
-    let msg = message;
-    if(msg.content === 'hi'){
-        msg.channel.send(`Hello ${message.author.tag}`)
-    }
-    if(msg.content === `${prefix}test`){
-        msg.author.send('**Hello User, Whats up?**')
-        msg.channel.send('**Check DM**')
-    }
-})
- 
+ bot.on("ready", function (){
+    console.log(`${bot.user.username} successfully logged in!`)
+});
+
 bot.login(process.env.token);
