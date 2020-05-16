b782c5ae58c9e08c6b4772d0683a6d6b0819dd73
Marko05 <Markoklincov781@gmail.com>
2020-05-16 17:34:59 +0200

    2121

--- index.js
+++ index.js
@@ -1,89 +1,27 @@
 const Discord = require('discord.js');
-const bot = new Discord.Client();
-const prefix = '!';
-const token = `NzA1OTA3NjQ2NDgxNDMyNjA4.Xr04lQ.ZEOhSxbeyP2Xce0WVXFMQWt2HNE`;
-const r = "RANDOM";
- 
-bot.on('ready', () => {
-    console.log(`${bot.user.tag} successfully logged in!`)
-    bot.user.setActivity('*help', ({type: "PLAYING"}))
+const client = new Discord.Client();
+
+client.on("ready", () => {
+  console.log('Online.')
 })
- 
-bot.on('message', message => {
-    let msg = message;
-    let args = msg.content.slice(prefix.length).split(/ +/);
-    let command = args.shift().toLowerCase();
-    let cmd = command;
- 
-    if (command === 'help') {
-        const embed = new Discord.MessageEmbed()
-        .setTitle('Commands')
-        .addField('General', `${prefix}help - Shows this message.\n${prefix}random - Shows a random number from <Args> to <args 2>`)
-        .setColor(0xff0000);
-        msg.channel.send(embed);
-    }
-    if (command === 'random') {
-        if(!args[0]) return msg.reply("You didn't specify args 1")
-        if(!args[1]) return msg.reply("You didn't specify args 2")
-        msg.channel.send("Your random number is: " + Math.floor(Math.random() * args[1] + args[0]));
-    }
-    if (cmd === 'clear' || cmd === 'purge'){
-        if(!msg.member.hasPermission("MANAGE_MESSAGES")) return msg.channel.send("**You can't use this command!**");
-        if(!args[0]) return msg.channel.send("**Specify how many messages you want to delete.**");
-        msg.delete();
-        msg.channel.bulkDelete(args[0]).catch(e => { msg.channel.send("**You can only delete 100 messages at once.**")});
-        msg.channel.send(`Successfully deleted \`${args[0]} messages\``).then(m => m.delete({ timeout: 5000 }));
-    }
-    if(cmd === 'kick'){
-        if(!msg.member.hasPermission('KICK_MEMBERS')) return msg.channel.send("**:x: You don't have permission to kick members.**");
-        let toKick = msg.mentions.members.first();
-        let reason = args.slice(1).join(" ");
-        if(!args[0]) return msg.channel.send('**:exclamation:Please mention someone to kick**');
-        if(!toKick) return msg.channel.send(`**:x:${args[0]} is not a member**`);
-        if(!reason) return msg.channel.send('**Specify a reason.**');
- 
-        if(!toKick.kickable){
-            return msg.channel.send('**:x: I cannot kick someone that is Mod / Admin.**');
-        }
- 
-        if(toKick.kickable){
-            let x = new Discord.MessageEmbed()
-            .setTitle('Kick')
-            .addField('Member Kicked', toKick)
-            .addField('Kicked by', msg.author)
-            .addField('Reason', reason)
-            .addField('Date', msg.createdAt)
-            .setColor(r);
- 
-            msg.channel.send(x);
-            toKick.kick();
-        }
-    }
-    if(cmd === 'ban'){
-        if(!msg.member.hasPermission("BAN_MEMBERS")) return msg.channel.send("**:x: You don't have permission to ban members.**");
-        let toBan = msg.mentions.members.first();
-        let reason = args.slice(1).join(" ");
-        if(!args[0]) return msg.channel.send('**:exclamation:Please mention someone to ban**');
-        if(!toBan) return msg.channel.send(`**:x: ${args[0]} is not a member.**`);
-        if(!reason) return msg.channel.send('**Specify a reason.**');
- 
-        if(!toBan.bannable){
-            return msg.channel.send(':x: I cannot ban someone that is Mod / Admin.');
-        }
- 
-        if(toBan.bannable){
-            let x = new Discord.MessageEmbed()
-            .setTitle('Ban')
-            .addField('Member Banned', toBan)
-            .addField('Banned by', msg.author)
-            .addField('Reason', reason)
-            .addField('Date', msg.createdAt)
-            .setColor(r);
- 
-            msg.channel.send(x);
-            toBan.ban();
-        }
-    }
-})
- 
-bot.login(token);
+
+client.on('message', async message => {
+  if (message.author.bot) return;
+  let prefix = '*'; // customized. you can change it whatever you want.
+  if (!message.content.startsWith(prefix)) return; // use this. so your bot will be only executed with prefix.
+  
+  let args = message.content.slice(prefix.length).trim().split(/ +/g);
+  let msg = message.content.toLowerCase();
+  let cmd = args.shift().toLowerCase();
+  
+  if (msg.startsWith(prefix + 'ping')) {
+    message.channel.send('pong.'); // results.
+  } // easy way.
+  
+  if (msg.startsWith(prefix + 'hello')) {
+    message.channel.send('hewwo'); // results.
+  }
+});
+
+client.login(process.NzA1OTA3NjQ2NDgxNDMyNjA4.XsAHqA.CkCaTIa-gZq0iXwUFrzAsX8f-Zw.TOKEN); // Put your token into the .env
+// Make sure to lock your project. Go to the your name project and click "Make This Project Private"
