5b7e2b0df703ac31bff1ac2bcca6b609a8aa0a09
Marko05 <Markoklincov781@gmail.com>
2020-05-17 23:29:49 +0200

    424

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const prefix = '*';
-const token = `NzA1OTA3NjQ2NDgxNDMyNjA4.XsAO3A.6s-YWmdGPv0BVfpCXipaEaWLosw`;
+const token = `NzA1OTA3NjQ2NDgxNDMyNjA4.XsGduw.GiONOVLJmEZVhaNQZfJ7kAjcd7I`;
 const r = "RANDOM";
  
 bot.on('ready', () => {
@@ -14,13 +14,11 @@ bot.on('message', message => {
     let args = msg.content.slice(prefix.length).split(/ +/);
     let command = args.shift().toLowerCase();
     let cmd = command;
-
-    if (!message.content.startsWith(prefix)) return;
  
     if (command === 'help') {
         const embed = new Discord.MessageEmbed()
         .setTitle('Commands')
-        .addField('General', `**${prefix}help - Shows this message.\n${prefix}random - Shows a random number from <Args> to <args 2>\n${prefix}server - Shows the name and the amount of Members on the server\n${prefix}user-info - Shows your username and your ID\n${prefix}avatar - Shows your avatar**`)
+        .addField('General', `**${prefix}help - Shows this message.\n${prefix}random - Shows a random number from <Args> to <args 2>.\n${prefix}server - Shows the name and the amount of Members on the server.\n${prefix}user-info - Shows your Username and your ID.\n${prefix}avatar - Shows your Avatar.**`)
         .setColor(0xff0000);
         msg.channel.send(embed);
     }
@@ -33,19 +31,19 @@ bot.on('message', message => {
         if(!msg.member.hasPermission("MANAGE_MESSAGES")) return msg.channel.send("**:x: You can't use this command!**");
         if(!args[0]) return msg.channel.send("**Specify how many messages you want to delete.**");
         msg.delete();
-        msg.channel.bulkDelete(args[0]).catch(e => { msg.channel.send("**You can only delete 100 messages at once.**")});
-        msg.channel.send(`**:white_check_mark: Successfully deleted \`${args[0]} messages\`**`).then(m => m.delete({ timeout: 8000 }));
+        msg.channel.bulkDelete(args[0]).catch(e => { msg.channel.send("You can only delete 100 messages at once.")});
+        msg.channel.send(`**:white_check_mark: Successfully deleted \`${args[0]} messages\`**`).then(m => m.delete({ timeout: 5000 }));
     }
     if(cmd === 'kick'){
         if(!msg.member.hasPermission('KICK_MEMBERS')) return msg.channel.send("**:x: You don't have permission to kick members.**");
         let toKick = msg.mentions.members.first();
         let reason = args.slice(1).join(" ");
-        if(!args[0]) return msg.channel.send('**Please mention someone to kick**');
-        if(!toKick) return msg.channel.send(`**:x: ${args[0]} is not a member.**`);
+        if(!args[0]) return msg.channel.send('**Please mention someone to kick.**');
+        if(!toKick) return msg.channel.send(`**${args[0]} is not a member.**`);
         if(!reason) return msg.channel.send('**Specify a reason.**');
  
         if(!toKick.kickable){
-            return msg.channel.send('**:x: I cannot kick someone that is mod/admin.**');
+            return msg.channel.send(':x: I cannot kick someone that is mod/admin.');
         }
  
         if(toKick.kickable){
@@ -65,12 +63,12 @@ bot.on('message', message => {
         if(!msg.member.hasPermission("BAN_MEMBERS")) return msg.channel.send("**:x: You don't have permission to ban members.**");
         let toBan = msg.mentions.members.first();
         let reason = args.slice(1).join(" ");
-        if(!args[0]) return msg.channel.send('**Please mention someone to ban**');
-        if(!toBan) return msg.channel.send(`**:x: ${args[0]} is not a member.**`);
+        if(!args[0]) return msg.channel.send('**Please mention someone to ban.**');
+        if(!toBan) return msg.channel.send(`**${args[0]} is not a member.**`);
         if(!reason) return msg.channel.send('**Specify a reason.**');
  
         if(!toBan.bannable){
-            return msg.channel.send('**:x: I cannot ban someone that is mod/admin.**');
+            return msg.channel.send(':x: I cannot ban someone that is mod/admin.');
         }
  
         if(toBan.bannable){
@@ -100,6 +98,6 @@ bot.on('message', message => {
     
         // ...
     }
-})
- 
+});
+
 bot.login(token);
