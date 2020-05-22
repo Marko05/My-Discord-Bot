09253db1205fa57827dbef77d85628ab02c94dd6
Marko05 <Markoklincov781@gmail.com>
2020-05-22 23:10:22 +0200

    3113

--- commands/giveaway.js
+++ commands/giveaway.js
@@ -4,20 +4,20 @@ const ms = require('ms');
 exports.run = async(client, msg, args) => {
 
     if(!args[0]) return msg.channel.send(`:x: **Please specify your time!**`)
-    if(!args[0].endsWith("d")&&!args[0].endsWith("h")&&!args[0].endsWith("m")) return msg.channel.send(`**You did not use the correct formatting for the time!**`)
+    if(!args[0].endsWith("d")&&!args[0].endsWith("h")&&!args[0].endsWith("m")) return msg.channel.send(`**You did not use the correct formatting for the time.**`)
     if(isNaN(args[0][0])) return msg.channel.send(`:x: **That is not a number!**`)
     
     let channel = msg.mentions.channels.first()
-    if(!channel) return msg.channel.send(`:x: **I could not find that channel in the guild!**`)
+    if(!channel) return msg.channel.send(`:x: **Please select a Channel.**`)
     
     let prize = args.slice(2).join(" ")
-    if(!prize) return msg.channel.send(`**No prize specified!**`)
+    if(!prize) return msg.channel.send(`**Please specify a price.**`)
     msg.channel.send(`**Giveaway created in ${channel}.**`)
     
     let Embed = new MessageEmbed()
     .setTitle(`**🎉 GIVEAWAY 🎉**`)
     .setDescription(`**${prize}**`)
-    .setFooter(`Ends:`, Date.now()+ms(args[0]))
+    .setTimestamp(Date.now()+ms(args[0]))
     .setColor(`BLUE`)
     let m = await channel.send(Embed)
     m.react("🎉")
