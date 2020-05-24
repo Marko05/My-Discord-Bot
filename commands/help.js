57bee9bf03db530bfa82f3be0d48b1c02628c654
Marko05 <Markoklincov781@gmail.com>
2020-05-24 16:19:02 +0200

    212

--- commands/help.js
+++ commands/help.js
@@ -11,7 +11,7 @@ exports.run = async(client, msg, args) => {
     .setThumbnail (`http://1.bp.blogspot.com/-zHadsvGwqEM/T48RSwyXEII/AAAAAAAAAOk/GrsTqyDwKWM/s1600/Crying-Anime-Girl-anime-girls-7642956-800-600.jpg`)
     .addField(`**Administrator**`, `**${prefix}ban @user <reason> - Bans a user from the Server.\n${prefix}kick @user <reason> - Kicks a user from the Server.\n${prefix}warn @user <reason> - Warns a user. \n${prefix}clear | purge <amount of messages> - Clears a amount of messages.(Max. 100)\n${prefix}giveaway <time> #channel <prize> - Creates a Giveaway.**`)
     .addField('**Infos**', `**${prefix}covid - Gives you Informations about Corona in the world.\n${prefix}covidstate <state> - Gives you Informations about Corona from a state.\n${prefix}pokemon <pokemon> - Gives you Informations about the pokemon.\n${prefix}user-info - Gives you Informations about your Account.\n${prefix}server-info - Gives you some Informations about the Server.\n${prefix}youtube - Shows Magg´s Youtube Channel.**`)
-    .addField(`**Economy**`, `**${prefix}bank - Shows your coins.\n${prefix}daily - Gives you 100 coins.` )
+    .addField(`**Economy**`, `**${prefix}bank - Shows your coins.\n${prefix}daily - Gives you 100 coins.**`)
     .addField(`**Fun**`, `**${prefix}meme - Shows you a random meme.\n${prefix}random - Shows a random number from 1 to ∞.\n${prefix}joke - Tells you a joke.\n${prefix}cnjoke - Tells you a Chuck Norris joke.\n${prefix}8ball <question> - Answers a question from you.\n${prefix}love <person/object> - Shows the love to the person/object.**`)
     .addField(`**Timer**`, `**${prefix}timer <time> - Sets a Timer for a specific time.**`)
     .addField(`**Utility**`, `**${prefix}invite - Invites the bot to your server.**`)
